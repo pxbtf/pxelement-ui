610a57277a8d028bb4fc22bc5b38f6b492763b96
pxbtf <pxiou1997@163.com>
2024-04-13 23:16:28 +0800

    chore:打包

---
+++ packages/components/dist/pxElement.mjs
@@ -0,0 +1,75 @@
+import { defineComponent, openBlock, createElementBlock, normalizeClass, createCommentVNode, renderSlot } from "vue";
+const buttonProps = {
+  type: {
+    type: String,
+    default: "default"
+  },
+  plain: {
+    type: Boolean,
+    default: false
+  },
+  round: {
+    type: Boolean,
+    default: false
+  },
+  circle: {
+    type: Boolean,
+    default: false
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  icon: {
+    type: String,
+    default: ""
+  }
+};
+const _sfc_main = defineComponent({
+  name: "pxbutton",
+  props: buttonProps,
+  emits: ["click"],
+  setup(_, { emit }) {
+    function clickHandle(e) {
+      emit("click", e);
+    }
+    return {
+      clickHandle
+    };
+  }
+});
+const _export_sfc = (sfc, props) => {
+  const target = sfc.__vccOpts || sfc;
+  for (const [key, val] of props) {
+    target[key] = val;
+  }
+  return target;
+};
+const _hoisted_1 = ["disabled"];
+const _hoisted_2 = { key: 1 };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("button", {
+    class: normalizeClass(["pxui-button", [`pxui-button-${_ctx.type}`, { "is-plain": _ctx.plain, "is-round": _ctx.round, "is-circle": _ctx.circle, "is-disabled": _ctx.disabled }]]),
+    disabled: _ctx.disabled,
+    onClick: _cache[0] || (_cache[0] = (...args) => _ctx.clickHandle && _ctx.clickHandle(...args))
+  }, [
+    _ctx.icon ? (openBlock(), createElementBlock("i", {
+      key: 0,
+      class: normalizeClass(`pxui-icon-${_ctx.icon}`)
+    }, null, 2)) : createCommentVNode("", true),
+    _ctx.$slots.default ? (openBlock(), createElementBlock("span", _hoisted_2, [
+      renderSlot(_ctx.$slots, "default")
+    ])) : createCommentVNode("", true)
+  ], 10, _hoisted_1);
+}
+const Button = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+const components = [Button];
+const install = (app) => {
+  components.forEach((component) => {
+    app.component(component.name, component);
+  });
+};
+const pxelement = { install };
+export {
+  pxelement as default
+};
---
+++ packages/components/dist/pxElement.umd.js
@@ -0,0 +1,78 @@
+(function(global, factory) {
+  typeof exports === "object" && typeof module !== "undefined" ? factory(exports, require("vue")) : typeof define === "function" && define.amd ? define(["exports", "vue"], factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, factory(global.pxElement = {}, global.Vue));
+})(this, function(exports2, vue) {
+  "use strict";
+  const buttonProps = {
+    type: {
+      type: String,
+      default: "default"
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String,
+      default: ""
+    }
+  };
+  const _sfc_main = vue.defineComponent({
+    name: "pxbutton",
+    props: buttonProps,
+    emits: ["click"],
+    setup(_, { emit }) {
+      function clickHandle(e) {
+        emit("click", e);
+      }
+      return {
+        clickHandle
+      };
+    }
+  });
+  const _export_sfc = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+      target[key] = val;
+    }
+    return target;
+  };
+  const _hoisted_1 = ["disabled"];
+  const _hoisted_2 = { key: 1 };
+  function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+    return vue.openBlock(), vue.createElementBlock("button", {
+      class: vue.normalizeClass(["pxui-button", [`pxui-button-${_ctx.type}`, { "is-plain": _ctx.plain, "is-round": _ctx.round, "is-circle": _ctx.circle, "is-disabled": _ctx.disabled }]]),
+      disabled: _ctx.disabled,
+      onClick: _cache[0] || (_cache[0] = (...args) => _ctx.clickHandle && _ctx.clickHandle(...args))
+    }, [
+      _ctx.icon ? (vue.openBlock(), vue.createElementBlock("i", {
+        key: 0,
+        class: vue.normalizeClass(`pxui-icon-${_ctx.icon}`)
+      }, null, 2)) : vue.createCommentVNode("", true),
+      _ctx.$slots.default ? (vue.openBlock(), vue.createElementBlock("span", _hoisted_2, [
+        vue.renderSlot(_ctx.$slots, "default")
+      ])) : vue.createCommentVNode("", true)
+    ], 10, _hoisted_1);
+  }
+  const Button = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+  const components = [Button];
+  const install = (app) => {
+    components.forEach((component) => {
+      app.component(component.name, component);
+    });
+  };
+  const pxelement = { install };
+  exports2.default = pxelement;
+  Object.defineProperties(exports2, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
+});
